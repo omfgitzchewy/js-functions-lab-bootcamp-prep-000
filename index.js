63d56e47f24278f2b13a8c293ba8b9e8d32c18d0
Aaron Chu <aaronchu357@gmail.com>
2019-01-16 23:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
 
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
+
 function holidayCountdown(days, holiday){
   return "It's ${days} days until ${holiday}!"
 }
